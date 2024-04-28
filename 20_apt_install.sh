155ac342ed364ff111ce1130b428cf98502ee6ae
dego1985 <>
2024-04-28 10:17:17 +0900

    add yes

--- 20_apt_install.sh
+++ 20_apt_install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+
+set -eu
+
 sudo apt-get update
-sudo apt-get install build-essential cmake git unzip pkg-config
-sudo apt-get install libjpeg-dev libpng-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
-sudo apt-get install libgstreamer1.0-dev gstreamer1.0-gtk3
-sudo apt-get install libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install python3-dev python3-numpy python3-pip
-sudo apt-get install libtbb2 libtbb-dev libdc1394-22-dev
-sudo apt-get install libv4l-dev v4l-utils
-sudo apt-get install libopenblas-dev libatlas-base-dev libblas-dev
-sudo apt-get install liblapack-dev gfortran libhdf5-dev
-sudo apt-get install libprotobuf-dev libgoogle-glog-dev libgflags-dev
-sudo apt-get install protobuf-compiler
-sudo apt-get install qtbase5-dev qtchooser
-sudo apt-get install qt5-qmake qtbase5-dev-tools
-sudo apt-get install qtcreator
-sudo apt-get install qtdeclarative5-dev
+sudo apt-get install -y build-essential cmake git unzip pkg-config
+sudo apt-get install -y libjpeg-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install -y libgtk2.0-dev libcanberra-gtk* libgtk-3-dev
+sudo apt-get install -y libgstreamer1.0-dev gstreamer1.0-gtk3
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev gstreamer1.0-gl
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y python3-dev python3-numpy python3-pip
+sudo apt-get install -y libtbb2 libtbb-dev libdc1394-22-dev
+sudo apt-get install -y libv4l-dev v4l-utils
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev
+sudo apt-get install -y liblapack-dev gfortran libhdf5-dev
+sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y protobuf-compiler
+sudo apt-get install -y qtbase5-dev qtchooser
+sudo apt-get install -y qt5-qmake qtbase5-dev-tools
+sudo apt-get install -y qtcreator
+sudo apt-get install -y qtdeclarative5-dev
