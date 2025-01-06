6896ae9aa5d777a2b5a250f8bcd8630f331d99eb
dego1985 <23418415+dego1985@users.noreply.github.com>
2025-01-06 19:28:13 +0900

    Update 20_apt_install.sh

--- 20_apt_install.sh
+++ 20_apt_install.sh
@@ -32,3 +32,6 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 echo "deb [trusted=yes] https://download.eclipse.org/zenoh/debian-repo/ /" | sudo tee -a /etc/apt/sources.list > /dev/null
 sudo apt update
 sudo apt install zenoh
+
+# for camera
+sudo apt install libcamera-dev
